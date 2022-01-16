709c4e59dc78f463089612a4c63c5c645171eaf3
James <jamesg487@gmail.com>
2022-01-16 12:24:05 +0000

    finished writing functions in src file and teseted all functions

---
+++ src/pet_shop.py
@@ -0,0 +1,73 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash_amount):
+    pet_shop["admin"]["total_cash"] += cash_amount
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, pets_sold):
+    pet_shop["admin"]["pets_sold"] += pets_sold
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+
+def get_pets_by_breed(pet_shop, breed):
+    pets = []
+    for pet in pet_shop["pets"]:
+            if pet["breed"] == breed:
+                pets.append(pet)
+    return pets
+
+def find_pet_by_name(pet_shop, pet_name):
+    pets_name = None
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            pets_name = pet
+    return pets_name
+
+def remove_pet_by_name(pet_shop, pet_name):
+    remove_pet = None
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            remove_pet = pet
+    pet_shop["pets"].remove(remove_pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    else:
+        return False
+
+def sell_pet_to_customer(pet_shop,pet,customer):
+    if pet == None:
+        return None
+    elif customer["cash"] < pet["price"]:
+        return None
+    else:
+        customer["pets"].append(pet)
+        pet_shop["admin"]["pets_sold"] += 1
+        customer["cash"] -= pet["price"]
+        pet_shop["admin"]["total_cash"] += pet["price"]
+    
